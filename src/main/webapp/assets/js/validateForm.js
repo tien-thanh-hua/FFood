8cd6f8bdce39c8d33d3bfe25ae4f6163240fed4f
hua-tien-thanh <huatienthanh2003@gmail.com>
2023-07-11 15:59:38 +0700

    Add validation to User management forms Change validation text to red color Replace the JSTL approach for handle dynamic update/delete forms with JavaScript approach, minimizing the number of modals generated per page load

--- src/main/webapp/assets/js/validateForm.js
+++ src/main/webapp/assets/js/validateForm.js
@@ -2,10 +2,9 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Other/javascript.js to edit this template
  */
-$(document).ready(validateForm());
+$(document).ready(validateForm);
 function validateForm() {
-
-  $("#add-food-form").validate({
+  $(".add-food-form").validate({
     rules: {
       txtFoodTypeID: {
         required: true
@@ -33,24 +32,24 @@ function validateForm() {
       }
     },
     messages: {
-      txtFoodTypeID: "Hãy chọn loại món",
+      txtFoodTypeID: "Vui lòng chọn loại món",
       txtFoodName: {
         required: "Tên món không được để trống",
         maxlength: "Tên món không dài quá 50 kí tự"
       },
       txtFoodPrice: {
-        required: "Đơn giá không được để trống",
+        required: "Vui lòng nhập Đơn giá",
         number: "Đơn giá phải là định dạng số",
         min: "Đơn giá phải là số dương"
       },
       txtDiscountPercent: {
-        required: "Giá trị giảm giá không được để trống",
+        required: "Vui lòng nhập Giá trị giảm giá",
         digits: "Giá trị giảm giá phải là số nguyên",
         min: "Giá trị giảm giá không được dưới 0",
         max: "Giá trị giảm giá không được quá 100"
       },
       txtImageURL: {
-        required: "Đường dẫn ảnh không được để trống",
+        required: "Vui lòng nhập Đường dẫn ảnh",
         url: "Giá trị nhập vào phải là đường dẫn",
         maxlength: "Đường dẫn không dài quá 255 kí tự",
         pattern: "Định dạng đường dẫn không đúng"
@@ -67,8 +66,8 @@ function validateForm() {
       form.submit();
     }
   });
-  
-  $("#update-food-form").validate({
+
+  $(".update-food-form").validate({
     rules: {
       txtFoodTypeID: {
         required: true
@@ -96,24 +95,26 @@ function validateForm() {
       }
     },
     messages: {
-      txtFoodTypeID: "Hãy chọn loại món",
+      txtFoodTypeID: {
+        required: "Vui lòng chọn loại món"
+      },
       txtFoodName: {
         required: "Tên món không được để trống",
         maxlength: "Tên món không dài quá 50 kí tự"
       },
       txtFoodPrice: {
-        required: "Đơn giá không được để trống",
+        required: "Vui lòng nhập Đơn giá",
         number: "Đơn giá phải là định dạng số",
         min: "Đơn giá phải là số dương"
       },
       txtDiscountPercent: {
-        required: "Giá trị giảm giá không được để trống",
+        required: "Vui lòng nhập Giá trị giảm giá",
         digits: "Giá trị giảm giá phải là số nguyên",
         min: "Giá trị giảm giá không được dưới 0",
         max: "Giá trị giảm giá không được quá 100"
       },
       txtImageURL: {
-        required: "Đường dẫn ảnh không được để trống",
+        required: "Vui lòng nhập Đường dẫn ảnh",
         url: "Giá trị nhập vào phải là đường dẫn",
         maxlength: "Đường dẫn không dài quá 255 kí tự",
         pattern: "Định dạng đường dẫn không đúng"
@@ -131,38 +132,102 @@ function validateForm() {
     }
   });
 
-  $("#login-form").validate({
+  $(".add-user-form").validate({
     rules: {
-      txtUsername: {
+      txtAccountUsername: {
         required: true,
-        maxlength: 50
+        maxlength: 50,
+        pattern: /^[a-zA-Z0-9-']+$/
       },
-      txtPassword: {
-        required: true
-      }
+      txtEmail: {
+        required: true,
+        maxlength: 255,
+        email: true
+      },
+      txtAccountPassword: {
+        required: true,
+        minlength: 8
+      },
+      txtAccountRePassword: {
+        required: true,
+        minlength: 8,
+        equalTo: "#txtAccountPassword"
+      },
     },
-
     messages: {
-      txtUsername: {
-        required: "Please enter the username.",
-        maxlength: "Username length should not exceed 50 characters."
+      txtAccountUsername: {
+        required: "Vui lòng nhập Tên Tài khoản Người dùng",
+        maxlength: "Tên Tài khoản Người dùng không được vượt quá 50 ký tự",
+        pattern: "Tên Tài khoản Người dùng chỉ chấp nhận chữ, số, dấu gạch ngang và dấu nháy đơn"
       },
-      txtProdName: {
-        required: "Please enter the password."
+      txtEmail: {
+        required: "Vui lòng nhập Email",
+        maxlength: "Email không được vượt quá 255 ký tự",
+        email: "Vui lòng nhập địa chỉ Email hợp lệ"
+      },
+      txtAccountPassword: {
+        required: "Vui lòng nhập Mật khẩu",
+        minlength: "Mật khẩu phải có ít nhất 8 ký tự"
+      },
+      txtAccountRePassword: {
+        required: "Vui lòng nhập lại Mật khẩu",
+        minlength: "Mật khẩu phải có ít nhất 8 ký tự",
+        equalTo: "Mật khẩu không khớp"
       }
-    },
-    errorElement: "p",
-    highlight: function (element, errorClass, validClass) {
-      $(element).parents(".col-sm-5").addClass("has-error").removeClass("has-success");
-    },
-    unhighlight: function (element, errorClass, validClass) {
-      $(element).parents(".col-sm-5").addClass("has-success").removeClass("has-error");
     },
     submitHandler: function (form) {
+      // Handle form submission here
+      form.submit();
+    }
+  });
+
+  $(".update-user-form").validate({
+    rules: {
+      txtAccountUsername: {
+        required: true,
+        maxlength: 50,
+        pattern: /^[a-zA-Z0-9-']+$/
+      },
+      txtEmail: {
+        required: true,
+        maxlength: 255,
+        email: true
+      },
+      txtAccountPassword: {
+        required: true,
+        minlength: 8
+      },
+      txtAccountRePassword: {
+        required: true,
+        minlength: 8,
+        equalTo: "#txtAccountPassword"
+      },
+    },
+    messages: {
+      txtAccountUsername: {
+        required: "Vui lòng nhập Tên Tài khoản Người dùng",
+        maxlength: "Tên Tài khoản Người dùng không được vượt quá 50 ký tự",
+        pattern: "Tên Tài khoản Người dùng chỉ chấp nhận chữ, số, dấu gạch ngang và dấu nháy đơn"
+      },
+      txtEmail: {
+        required: "Vui lòng nhập Email",
+        maxlength: "Email không được vượt quá 255 ký tự",
+        email: "Vui lòng nhập địa chỉ Email hợp lệ"
+      },
+      txtAccountPassword: {
+        required: "Vui lòng nhập Mật khẩu",
+        minlength: "Mật khẩu phải có ít nhất 8 ký tự"
+      },
+      txtAccountRePassword: {
+        required: "Vui lòng nhập lại Mật khẩu",
+        minlength: "Mật khẩu phải có ít nhất 8 ký tự",
+        equalTo: "Mật khẩu không khớp"
+      }
+    },
+    submitHandler: function (form) {
+      // Handle form submission here
       form.submit();
     }
   });
 
 }
-
-
